95e481b927d4320cfbd865ae1357ed01b04e2599
Alessandro Clerici <ale.clericil@gmail.com>
2021-06-27 13:07:05 +0200

    invert-screen now fixes pen too

--- scripts/.scripts/src/invert-screen.sh
+++ scripts/.scripts/src/invert-screen.sh
@@ -6,6 +6,7 @@ if [ -z $MONITOR_L ]; then
 fi
 
 TOUCH_SCREEN=$(xinput list | sed -n 's/.*Finger.*id=\([0-9]\+\).*/\1/p')
+PEN=$(xinput list | sed -n 's/.*Pen Pen.*id=\([0-9]\+\).*/\1/p')
 
 if [ -f /tmp/inverted-screen ]; then
 	xrandr --output $MONITOR_L --rotate normal
@@ -15,3 +16,4 @@ else
 	touch /tmp/inverted-screen
 fi
 xinput map-to-output $TOUCH_SCREEN $MONITOR_L
+xinput map-to-output $PEN $MONITOR_L
