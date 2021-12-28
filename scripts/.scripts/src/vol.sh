e74c9792822d7b231f6c18f41f744e65c4a6eb11
Alessandro Clerici <ale.clericil@gmail.com>
2021-12-28 20:32:19 +0100

    vol script: now using pactl (less dependencies)

--- scripts/.scripts/src/vol.sh
+++ scripts/.scripts/src/vol.sh
@@ -8,6 +8,6 @@
 vol=$(echo $1 | cut -d % -f 1)
 
 # pulseaudio command (for each sink)
-for i in $(pacmd list-sinks | grep index | cut -d : -f 2); do
+for i in $(pactl list sinks short | cut -f1); do
 	pactl set-sink-volume $i $vol%
 done
