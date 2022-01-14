c7ed1d8290ad117bcc5896511245534fb64088cd
Alessandro Clerici <ale.clericil@gmail.com>
2022-01-15 00:05:33 +0100

    scripts update

--- scripts/.scripts/src/vol.sh
+++ scripts/.scripts/src/vol.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Sgorblex's scripts
 # ~/.scripts/vol.sh
-# This script set all pulseaudio sinks' volume to the value percentage specified in the command-line argument (requires pulseaudio)
+# This script set default pulseaudio sinks' volume to the value percentage specified in the command-line argument (requires pulseaudio)
 
 
-vol=$(echo $1 | cut -d % -f 1)
-
-# pulseaudio command (for each sink)
-for i in $(pactl list sinks short | cut -f1); do
-	pactl set-sink-volume $i $vol%
-done
+pactl set-sink-volume @DEFAULT_SINK@ ${1%\%}%
