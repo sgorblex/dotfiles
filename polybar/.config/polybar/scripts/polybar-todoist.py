5fddd474ffaa03cdf22521e33a022691e6de5b5a
Alessandro Clerici <ale.clericil@gmail.com>
2022-10-15 19:18:30 +0200

    wm setup update

--- polybar/.config/polybar/scripts/polybar-todoist.py
+++ polybar/.config/polybar/scripts/polybar-todoist.py
@@ -3,56 +3,32 @@
 # ~/.config/polybar/scripts/polybar-todoist.py
 # Script for the todoist polybar module
 
-import datetime
-from json import loads as jsnld
+from datetime import date
 from subprocess import run
-from time import sleep
+from sys import stderr
 
-import todoist
-
-
-class APIKeyNotFoundError(Exception):
-    pass
-
-
-def api_token_bitwarden():
-    try:
-        for field in jsnld(run(["bw", "--nointeraction", "--raw", "list", "items", "--search", "todoist"], capture_output=True, text=True).stdout)[0]["fields"]:
-            if field["name"] == "API token":
-                return field["value"]
-    except:
-        raise APIKeyNotFoundError
-    raise APIKeyNotFoundError
+from todoist_api_python.api import TodoistAPI
 
 
 def api_token():
     return run(['secret-tool', 'lookup', 'uuid', 'todoist_api_token'], capture_output=True, text=True).stdout
 
 
-def filter_dueToday(item):
-    if item['checked'] or not item['due']:
-        return False
-
-    duedate = datetime.datetime.strptime(
-        item['due']['date'][0:10], '%Y-%m-%d').date()
-
-    return duedate <= datetime.date.today()
-
-
 def main():
-    try:
-        api = todoist.TodoistAPI(api_token())
-    except:
-        print('ERROR')
+    token = api_token()
+    if not token:
+        print("Can't find token")
         exit(1)
-    while True:
-        try:
-            api.sync()
-            res = len(api.items.all(filt=filter_dueToday))
-            print(res if res else " ")
-        except:
-            print("")
-        sleep(5)
+    api = TodoistAPI(token)
+    try:
+        tasks = api.get_tasks()
+        filtered = [t for t in tasks if not t.completed and t.due and t.due.date <= date.today().strftime("%Y-%m-%d")]
+        res = len(filtered)
+        print(res if res else " ")
+    except Exception as error:
+        print("Error")
+        #  print(error)
+        exit(1)
 
 
 main()
