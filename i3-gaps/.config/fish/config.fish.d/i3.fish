56f8f4f08ec40a153055c1153e94be6667650ed5
Alessandro Clerici <ale.clericil@gmail.com>
2022-12-07 17:09:21 +0100

    improvements to i3 config

---
+++ i3-gaps/.config/fish/config.fish.d/i3.fish
@@ -0,0 +1,7 @@
+# Sgorblex's Fish shell config
+# ~/.config/fish/config.fish.d/i3
+# i3 utilities
+
+
+# manually launch display setup
+alias display ~/.config/i3/scripts/display.sh
--- i3-gaps/.config/i3/scripts/i3-waitmove.sh
+++ i3-gaps/.config/i3/scripts/i3-waitmove.sh
@@ -4,18 +4,29 @@ set -e
 
 PROGRAM=${0##*/}
 USAGE="$PROGRAM: Move an i3 window as soon as it exists.
-Usage: $PROGRAM WINDOW_CLASS WORKSPACE [DELAY]
-Tries to move all windows of class WINDOW_CLASS in workspace WORKSPACE every DELAY seconds until the move succeeds."
+Usage: $PROGRAM [-c CLASS | -t TITLE] WORKSPACE [DELAY]
+Tries to move all windows that match the specified criterium in workspace WORKSPACE every DELAY seconds until the move succeeds.
 
-if [ $# -lt 2 -o $# -gt 3 -o "$1" = "--help" ]; then
+The criterium may be:
+-c CLASS	window class
+-t TITLE	window title"
+
+if [ $# -lt 3 -o $# -gt 4 -o "$1" = "--help" ]; then
 	printf "%s\n" "$USAGE"
 	exit 0
 fi
 
-class=$1
-workspace=$2
-sleeptime=${3:-0.5}
+querytype=$1
+querycontent=$2
+workspace=$3
+sleeptime=${4:-0.5}
 
-while ! i3-msg "[class=$class]" move workspace "$workspace"; do
+if [ $querytype = '-c' ]; then
+	query="[class=$querycontent]"
+elif [ $querytype = '-t' ]; then
+	query="[title=$querycontent]"
+fi
+
+while ! i3-msg "$query" move workspace "$workspace"; do
 	sleep $sleeptime
 done
