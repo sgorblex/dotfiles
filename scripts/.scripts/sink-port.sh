47e1b74e176eea967b40f6e3970ae3f10a4dca7d
Alessandro Clerici <ale.clericil@gmail.com>
2023-04-03 10:57:54 +0200

    audio script update w/ polybar integration

--- scripts/.scripts/sink-port.sh
+++ scripts/.scripts/sink-port.sh
@@ -5,13 +5,27 @@
 
 # set -e
 
-amixer -c 1 sset 'Auto-Mute Mode' Disabled
+# amixer -c 1 sset 'Auto-Mute Mode' Disabled
 DEFSINK=$(pactl get-default-sink)
 SINKJSON=$(pactl -f json list sinks | jq ".[] | select(.name == \"$DEFSINK\")")
 ACTIVEPORT=$(printf "%s" "$SINKJSON" | jq ".active_port")
-PORTS=$(printf "%s" "$SINKJSON" | jq "[.ports[].name]")
+PORTS=$(printf "%s" "$SINKJSON" | jq "[.ports[] | select(.availability == \"available\").name]")
 INDEXACTIVEPORT=$(printf "%s" "$PORTS" | jq "index($ACTIVEPORT)")
 NEWPORT=$(printf "%s" "$PORTS" | jq ".[($INDEXACTIVEPORT+1)% length]")
 NEWPORT="${NEWPORT%\"}"
 NEWPORT="${NEWPORT#\"}"
+[ -n "$DEBUG" ] && ( printf 'Variables:
+DEFSINK=%s
+ACTIVEPORT=%s
+PORTS=%s
+INDEXACTIVEPORT=%s
+NEWPORT=%s
+' \
+"$DEFSINK" \
+"$ACTIVEPORT" \
+"$PORTS" \
+"$INDEXACTIVEPORT" \
+"$NEWPORT"
+)
+[ -n "$DEBUG" ] && set -x
 pactl set-sink-port @DEFAULT_SINK@ $NEWPORT
